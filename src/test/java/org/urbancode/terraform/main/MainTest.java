dc04b856e7dc1be276b23a18ac1bba14a1e7129a
smm <smm@urbancode.com>
2012-06-21 17:16:08 -0400

    ok really adding the test class/folders now

---
+++ src/test/java/org/urbancode/terraform/main/MainTest.java
@@ -0,0 +1,23 @@
+package org.urbancode.terraform.main;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+
+    //**********************************************************************************************
+    // CLASS
+    //**********************************************************************************************
+
+    //**********************************************************************************************
+    // INSTANCE
+    //**********************************************************************************************
+
+    //----------------------------------------------------------------------------------------------
+    @Test
+    public void testMain() {
+        //this is a test to placate git for now
+        String howAwesomeIsSteven = "super awesome";
+        Assert.assertEquals(true, "super awesome".equals(howAwesomeIsSteven));
+    }
+}
